3450a9b3bd6aea430d32c90f7702ae2cb6b51923
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-01-27 05:51:06 +0000

    2024/01/27 (am)
    Classwork

---
+++ Python_Introduction/Chapter02/full_name.py
@@ -0,0 +1,8 @@
+first_name = "Jonathan"
+middle_name = "Babayega"
+last_name = "Wick"
+full_name = f"{first_name.title()} {middle_name.title()} {last_name.title()}"
+# print(full_name)
+
+print(f"\t\nHello, {full_name.title()}!")
+print("Languages:\n\tRussian \n\tEnglish \n\tMandarin \n\tDeath")
