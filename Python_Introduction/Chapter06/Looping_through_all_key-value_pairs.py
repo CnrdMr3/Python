570f2a08f2b2cb9c2e54751cf65ba00cc2430f23
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-02-05 11:11:43 +0000

    2023/02/05 (am)
    Chapter06

---
+++ Python_Introduction/Chapter06/Looping_through_all_key-value_pairs.py
@@ -0,0 +1,10 @@
+friends = {
+    'friend0': 'Tian',
+    'friend1': 'Erik',
+    'friend2': 'Johan',
+    'friend3': 'JR',
+}
+
+for key, value in friends.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
