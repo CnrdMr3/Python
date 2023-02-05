ec0579016833c7e136a39d415bf2e24aa7a843c9
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-02-05 14:04:42 +0000

    2023/02/05 (pm)
    Chapter08

---
+++ Python_Introduction/Chapter08/Python-Pycharm/greeter-funnyThreats.py
@@ -0,0 +1,35 @@
+def greet_user():
+    """Display a simple greeting."""
+    print("Welcome to your NIGHTMARE!")
+
+
+greet_user()
+
+
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+
+    # REMOVE THE f"Hello, ...". It sends kind of a mixed message! YOU ARE WELCOME!
+
+
+greet_user('sl-hacker')
+print('think....')
+greet_user('-ls cmd line is fits you better!')
+
+# THIS IS HOW TO DO IT!
+
+# def greet_user():
+#    """Display a simple greeting."""
+#    print("Hello!")
+
+
+# greet_user()
+
+
+# def greet_user(username):
+#    """Display a simple greeting."""
+#    print(f"Hello, {username.title()}!")
+
+
+# greet_user('jesse')
